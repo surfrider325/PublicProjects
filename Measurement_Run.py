2d29915afb8b43b5978e6287c418eb195192a016
surfrider325 <rcschaub@gmail.com>
2024-04-14 18:47:27 -0400

    Updated Measurement_Run.py

---
+++ Measurement_Run.py
@@ -0,0 +1,27 @@
+from prefect import flow
+from prefect.blocks.notifications import SlackWebhook
+from tabulate import tabulate
+
+import Indicators
+import Measurement
+import Charts
+
+slack_webhook_block = SlackWebhook.load("slack")
+
+M = 80
+K = 500
+window = 7
+smoothing = 7
+events = {'ihs_event':'bull','hs_event':'bear','fw_event':'bull','rw_event':'bear'}
+bound = 0.03
+SMAs = [30,60,90]
+
+@flow(log_prints=True)
+def Measurement_Run():
+    df3, final4 = Measurement.main(events, SMAs, smoothing, window, M, K, bound)
+    df_print = final4.sort_values(['event_end_time'],ascending=0).head(15)
+    
+    slack_webhook_block.notify(tabulate(df_print, tablefmt="grid"))
+    
+if __name__ == "__main__":
+    Measurement_Run()
