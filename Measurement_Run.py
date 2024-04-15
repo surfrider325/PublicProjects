eb89d571d8faddced173e9b2e6650c09b40b4da6
surfrider325 <rcschaub@gmail.com>
2024-04-15 19:56:25 -0400

    Updated Measurement_Run.py with another slack webhook fix

--- Measurement_Run.py
+++ Measurement_Run.py
@@ -20,7 +20,9 @@ SMAs = [30,60,90]
 def Measurement_Run():
     df3, final4 = Measurement.main(events, SMAs, smoothing, window, M, K, bound)
     df_print = final4.sort_values(['event_end_time'],ascending=0).head(15)
-    df_print.drop(['index','after_event_observations','after_event_mean','count','mean','min','median','max','Indicator'],axis=1,inplace=True)
+    df_print.drop(['index','after_event_observations','after_event_mean',
+                   'after_event_start_time','after_event_end_time',
+                   'count','mean','min','median','max','Indicator'],axis=1,inplace=True)
     
     slack_webhook_block.notify("```\n" + tabulate(df_print, tablefmt="grid") + "\n```")
     
